9f564779b978e12a23ee11a5a3101ab9f81ea6a6
Aitor Sola <solasola1206@hotmail.com>
2022-03-03 18:09:13 +0100

    Initial Commit

---
+++ Shared/Gas4OilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gas4OilApp.swift
+//  Shared
+//
+//  Created by Aitor Sola on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Gas4OilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
